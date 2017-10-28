0e3e21e604d0f7439e6cdf220fc26ffd5616c2ad
Petter Östergren <petter.ostergren@icloud.com>
2017-10-28 13:26:25 +0200

    refactor: UI sort button :/category

--- src/components/SortPosts.js
+++ src/components/SortPosts.js
@@ -5,17 +5,13 @@ import { Link, withRouter } from 'react-router-dom'
 class SortPosts extends Component {
   render() {
     const { match } = this.props
+    const urlMatch = match.path === '/' || match.params.category !== 'undefined'
+    console.log(match.params.category)
     return (
-      <div className={match.path === '/' ? 'sort-posts' : 'sort-comments'}>
-        <div
-          className={
-            match.path === '/' ? 'dropdown-sort' : 'dropdown-sort-comments'
-          }
-        >
+      <div className={urlMatch ? 'sort-posts' : 'sort-comments'}>
+        <div className={urlMatch ? 'dropdown-sort' : 'dropdown-sort-comments'}>
           <button
-            className={
-              match.path === '/' ? 'dropbtn-sort' : 'dropbtn-sort-comments'
-            }
+            className={urlMatch ? 'dropbtn-sort' : 'dropbtn-sort-comments'}
           >
             <i className="fa fa-caret-down" /> Sort by
           </button>
