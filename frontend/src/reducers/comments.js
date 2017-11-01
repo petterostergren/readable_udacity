7f76a84d11db3b235c4809c8a82dce98ea525c97
Petter Östergren <petter.ostergren@icloud.com>
2017-11-01 13:41:42 +0100

    fix: issue with comments edit reducer

--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -3,6 +3,7 @@ import {
   COMMENTS_POST_VOTE,
   COMMENTS_DEL_COMMENT,
   COMMENTS_ADD_COMMENT,
+  COMMENTS_EDIT_COMMENT,
 } from '../actions/actionConstants'
 
 const comments = (state = {}, action) => {
@@ -27,6 +28,12 @@ const comments = (state = {}, action) => {
       comments[indexComment] = payload
       const updateComment = { ...state, [postId]: comments }
       return updateComment
+    case COMMENTS_EDIT_COMMENT:
+      const commenToEdit = [...state[payload.parentId]]
+      const commenToEditIndex = commenToEdit.findIndex(({ id }) => payload.id)
+      return {
+        [payload.parentId]: [[commenToEditIndex], payload],
+      }
     case COMMENTS_ADD_COMMENT:
       return {
         state,
