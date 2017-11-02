828dcecdebb0d767d4c4ca8be42d152d9a3e9714
Petter Östergren <petter.ostergren@icloud.com>
2017-11-02 21:23:00 +0100

    fix: comment bug in reducer

--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -15,22 +15,29 @@ const comments = (state = {}, action) => {
         [action.meta]: payload,
       }
     case COMMENTS_POST_VOTE:
-      const { parentId } = payload // get commentId
+      console.log(action.payload.id)
+      const { parentId } = action.payload // get commentId
       const commentList = [...state[parentId]] // get array of comments, but copy it
-      const commentIndex = commentList.findIndex(({ id }) => payload.id) // get index of comment
-      commentList[commentIndex] = payload // update the commentList
+      const commentIndex = commentList.findIndex(
+        inList => inList.id === payload.id
+      ) // get index of comment
+      commentList[commentIndex] = action.payload // update the commentList
       const updatedPost = { ...state, [parentId]: commentList } // return new state
       return updatedPost
     case COMMENTS_DEL_COMMENT:
       const postId = payload.parentId
       const comments = [...state[postId]]
-      const indexComment = comments.findIndex(({ id }) => payload.id)
+      const indexComment = comments.findIndex(
+        inList => inList.id === payload.id
+      )
       comments[indexComment] = payload
       const updateComment = { ...state, [postId]: comments }
       return updateComment
     case COMMENTS_EDIT_COMMENT:
       const commenToEdit = [...state[payload.parentId]]
-      const commenToEditIndex = commenToEdit.findIndex(({ id }) => payload.id)
+      const commenToEditIndex = commenToEdit.findIndex(
+        inList => inList.id === payload.id
+      )
       return {
         [payload.parentId]: [[commenToEditIndex], payload],
       }
--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -13,7 +13,7 @@ const posts = (state = [], action) => {
     case POST_CREATE_POST:
       return [...state, payload]
     case POST_EDIT_POST:
-      const postIndex = state.findIndex(({ id }) => payload.id)
+      const postIndex = state.findIndex(item => item.id === payload.id)
       return [(state[postIndex]: payload)]
     case POST_VOTE_POST:
       const itemIndex = state.findIndex(item => item.id === payload.id)
